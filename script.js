ebd31f6d277339b2d011ede06e9f32c1862d83b3
Mahdi <mahdihs395@gmail.com>
2022-10-05 11:02:47 +0300

    fixed some bugs

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const headers = document.querySelectorAll('.page-header');
+//const navbar = document.querySelector('.navbar');
 
 function randomColor(){
   const r = Math.floor(Math.random() * 255) + 1;
@@ -16,3 +17,17 @@ headers.forEach((header) => {
     header.style.color = 'black'
   })
 })
+$(function(){
+  $(document).scroll(function(){
+    let $navbar = $('.navbar');
+    let $navBrand = $('.navbar-brand');
+    let navLinks = document.querySelectorAll('.nav-link');
+    $navbar.toggleClass("navbar-scrolled", $(this).scrollTop() > $navbar.height());
+    $navBrand.toggleClass("brand-scrolled", $(this).scrollTop() > $navbar.height());
+    navLinks.forEach(link => {
+      link.classList.toggle('link-scrolled', $(this).scrollTop() > $navbar.height())
+    })
+  })
+}
+
+)
